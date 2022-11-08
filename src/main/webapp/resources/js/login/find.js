152390c679708d07014c1ee85ff3c1ef560ead81
GR <GR@DESKTOP-ODLO1F7>
2022-11-08 16:52:53 +0900

    수정2

---
+++ src/main/webapp/resources/js/login/find.js
@@ -0,0 +1,21 @@
+/**
+ * 회원가입 관련 js
+ */
+
+function gg() {
+	if ($("#find_id").css('display') == 'block') {
+		$("#find_id").css('display', 'none');
+	} else {
+		$("#find_id").css('display', 'block');
+		$("#find_pwd").css('display', 'none');
+	}
+}
+
+function pwd() {
+	if ($("#find_pwd").css('display') == 'block') {
+		$("#find_pwd").css('display', 'none');
+	} else {
+		$("#find_pwd").css('display', 'block');
+		$("#find_id").css('display', 'none');
+	}
+}
